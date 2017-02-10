7e801652c0e7357f4246e5751cbe0f5c20ed746a
wangliang <wangliang@rhtop.cn>
2017-02-10 14:34:48 +0800

    第一次更新

--- app/src/main/java/com/rh/icetool/activity/SellFragment.java
+++ app/src/main/java/com/rh/icetool/activity/SellFragment.java
@@ -6,26 +6,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.rh.icetool.R;
+import com.rh.icetool.adapter.MyAdapter;
+import com.rh.icetool.javaBean.Goods;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SellFragment extends Fragment {
-
-
-    public SellFragment() {
-        // Required empty public constructor
-    }
-
+    private List<Goods> goodses = new ArrayList<>();
+    private ListView lv;
+    private MyAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sell_sell, container, false);
+        View view = inflater.inflate(R.layout.fragment_history_sell, container, false);
+        lv = (ListView) view.findViewById(R.id.lv_history);
+
+        //准备数据
+        goodses = prepare();
+
+        adapter = new MyAdapter(getActivity(), goodses);
+
+        lv.setAdapter(adapter);
+
+        return view;
     }
 
+    private List<Goods> prepare() {
+        Goods good1 = new Goods(34178266, "2017.1.2", "李建国", "猪手", 202, "300.00", 3, "");
+        Goods good2 = new Goods(45170018, "2017.1.2", "王爽", "筒子骨", 202, "280.00", 2, "");
+        Goods good3 = new Goods(34178122, "2017.1.2", "刘琴", "五花肉", 28, "400.00", 1, "");
+        Goods good4 = new Goods(34123567, "2017.1.2", "胡东", "猪手", 202, "290.00", 1, "");
+        goodses.add(good1);
+        goodses.add(good2);
+        goodses.add(good3);
+        goodses.add(good4);
+
+        return goodses;
+    }
 }
---
+++ app/src/main/java/com/rh/icetool/adapter/MfAdapter.java
@@ -0,0 +1,30 @@
+package com.rh.icetool.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Jeremy on 2017-02-10.
+ */
+
+public class MfAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments;
+
+    public MfAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
